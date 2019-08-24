73330115ba24865cf66a629f9fcc53289c6090ac
dingy007 <dingy007@gmail.com>
2019-08-25 00:48:33 +0800

    Using props from list of data

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 import ContactCard from "./ContactCard";
 import Joke from "./Joke";
+import todosData from './todosData';
 
 class MainContent extends Component {
   render() {
@@ -16,32 +17,32 @@ class MainContent extends Component {
       styles.color = "Red";
     }
 
-    let jokesData = [
-      {
-        id: 1,
-        question: "What is your name?",
-        answer: "That is funny"
-      },
-      {
-        id: 2,
-        question: "What is your age?",
-        answer: "That is funny too"
-      }
-    ];
+    let todosComponents = todosData.map(todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />);
 
-    let jokesComponents = jokesData.map(joke => (
-      <Joke key={joke.id} question={joke.question} answer={joke.answer} />
-    ));
+    // let jokesData = [
+    //   {
+    //     id: 1,
+    //     question: "What is your name?",
+    //     answer: "That is funny"
+    //   },
+    //   {
+    //     id: 2,
+    //     question: "What is your age?",
+    //     answer: "That is funny too"
+    //   }
+    // ];
+
+    // let jokesComponents = jokesData.map(joke => (
+    //   <Joke key={joke.id} question={joke.question} answer={joke.answer} />
+    // ));
 
     return (
       <React.Fragment>
         {/* <h1 style= { {color: "#ffcc00", backgroundColor: "blue"} }>Good {`${timeOfDay} ${firstName} ${lastName}`}</h1> */}
         <h1 style={styles}>Good {`${timeOfDay} ${firstName} ${lastName}`}</h1>
-        {/* <div className = "todo-list">
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
-        </div> */}
+        <div className="todo-list">
+            {todosComponents}
+        </div>
         {/* 
         <ContactCard
           contact={{
@@ -88,7 +89,7 @@ class MainContent extends Component {
             answer: "That is funny too"
           }}
         /> */}
-        {jokesComponents}
+        {/* {jokesComponents} */}
       </React.Fragment>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
-  render() {
+
+  render(props) {
     return (
       <div className="todo-item">
-        <input type="checkbox" />
-        <p>Placeholder text</p>
+        <input type="checkbox" checked={this.props.completed}/>
+        <p>{this.props.text}</p>
         <br />
       </div>
     );
