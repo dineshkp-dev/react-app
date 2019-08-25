37a6b3a97cd02f8e326744ecbb94eb45655e8fe3
dingy007 <dingy007@gmail.com>
2019-08-25 13:38:36 +0800

    Lifecycle methods for reference

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,6 +8,35 @@ class TodoItem extends Component {
     };
   }
 
+  componentDidMount() {
+    // run once
+    // do API calls
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // run when parent passes props or updates props
+    // can check the props and change component properties
+    // deprecated
+  }
+
+  shouldComponentUpdate(nextProps, nextProps) {
+    // return true to update
+    // return false to not-update
+  }
+
+  componentWillUnmount() {
+    // to do cleanup 
+    // eg. remove event listener
+  }
+
+  getDerivedStateFromProps(props, state) {
+    // return new updated state based on the props
+  }
+
+  getSnapshotBeforeUpdate() {
+    // can be used to create backup before update
+  }
+  
   render(props) {
     console.log(this.props);
     return (
